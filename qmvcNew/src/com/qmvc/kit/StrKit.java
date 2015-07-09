8d187dd89d80238218ab47e192c3a3fcba991c3c
qkkcoolmax <371571945@qq.com>
2015-07-09 17:34:37 +0800

    sdfs

--- qmvcNew/src/com/qmvc/kit/StrKit.java
+++ qmvcNew/src/com/qmvc/kit/StrKit.java
@@ -1,26 +1,22 @@
 package com.qmvc.kit;
 
-/**@author everxs/ez
- * �?��String的工具类，判断为空或者头字母大小写等�?
- * StrKit.
- */
 public class StrKit {
-	
+
 	/**
 	 * 判断首字母大小写
+	 * 
 	 * @param str
 	 * @return
 	 */
-	public static boolean firstCharIsUp(String str){
+	public static boolean firstCharIsUp(String str) {
 		char firstChar = str.charAt(0);
 		if (firstChar >= 'A' && firstChar <= 'Z') {
-		  //如果为大�?
+			// 如果为大�?
 			return true;
 		}
 		return false;
 	}
-	
-	
+
 	/**
 	 * 第一个字母大�?
 	 */
@@ -33,7 +29,7 @@ public class StrKit {
 		}
 		return str;
 	}
-	
+
 	/**
 	 * 第一个字母小�?
 	 */
@@ -46,21 +42,21 @@ public class StrKit {
 		}
 		return str;
 	}
-	
+
 	/**
 	 * 
 	 */
 	public static boolean isBlank(String str) {
 		return str == null || "".equals(str.trim()) ? true : false;
 	}
-	
+
 	/**
 	 * 
 	 */
 	public static boolean notBlank(String str) {
 		return str == null || "".equals(str.trim()) ? false : true;
 	}
-	
+
 	public static boolean notBlank(String... strings) {
 		if (strings == null)
 			return false;
@@ -69,7 +65,7 @@ public class StrKit {
 				return false;
 		return true;
 	}
-	
+
 	public static boolean notNull(Object... paras) {
 		if (paras == null)
 			return false;
@@ -78,18 +74,19 @@ public class StrKit {
 				return false;
 		return true;
 	}
-	
+
 	/**
-	 * 返回�?split 分割的字符串的尾�? 例如   abc.txt  返回 txt
+	 * 返回�?split 分割的字符串的尾�? 例如 abc.txt 返回 txt
+	 * 
 	 * @param content
 	 * @param split
 	 * @return
 	 */
-	public static String getEndType(String content,String split){
+	public static String getEndType(String content, String split) {
 		String ss[] = content.split(split);
-		return ss[ss.length-1];
+		return ss[ss.length - 1];
 	}
-	
+
 	public static void main(String[] args) {
 		String name = "abc.txt";
 		System.out.println(getEndType(name, "\\."));
